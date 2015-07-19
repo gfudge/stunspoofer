d7628a00fc940274f33a057dc0c9b933dbd00015
Ubuntu <ubuntu@ip-172-31-24-79.eu-west-1.compute.internal>
2015-07-19 21:27:09 +0000

    Partially functional loggin mechanism

--- StunServer.py
+++ StunServer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-import time, sys, SocketServer, threading, struct
+import time, sys, SocketServer, threading, struct, Queue
 from collections import namedtuple
+
+storeIPQueue = Queue.Queue()
 
 class StunServerThread (threading.Thread):
 	def __init__(self):
@@ -30,9 +32,10 @@ class StunHandler(SocketServer.BaseRequestHandler):
 			# IP from received packet, ignore port
 			userIP = self.client_address[0]
 			print "Got IP: " + str(userIP)
+			storeIPQueue.put(str(userIP).strip())
 			# Unpack as packet according to rfc5389
 			newStunPacket = StunPacket._make(struct.unpack('>HHi%ds' %(12), request[0:20]))
-			print str(newStunPacket)
+			#print str(newStunPacket)
 		except Exception,e:
 			print "Handler Error: "
 			print str(e)
@@ -63,8 +66,31 @@ class StunServer():
 			Thread.die()
 		sys.exit(1)
 
+class IPStorage(threading.Thread):
+	def __init__(self, fileName):
+		threading.Thread.__init__(self)
+		self.fileName = str(fileName)
+		self.IPList = []		
+
+	def run(self):
+		try:	
+			self.storageFile = open(self.fileName, 'a+')
+			while(True):
+				self.IPList = self.storageFile.readlines()
+				#print self.IPList
+				while not storeIPQueue.empty():	
+					tempIP = storeIPQueue.get()
+					print "DEBUG: " + str(tempIP)
+					if tempIP not in self.IPList:
+						self.storageFile.write(str(tempIP) + "\n")
+
+		except Exception,e:
+			print "File Error: " + str(e)
+			self.storageFile.close()		
 def main():
+	storage = IPStorage('caught_ips.txt')
 	stunServer = StunServer(maxThreads=4)
+	storage.start()
 	stunServer.run()
 
 if __name__== "__main__":
